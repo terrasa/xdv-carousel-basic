08a0fd39b5dbd452451492159468665f88fd443b
Luis Terrasa <terrasa.luis@gmail.com>
2023-05-31 13:17:01 +0200

    wip

--- src/XdvCarouselBasic.js
+++ src/XdvCarouselBasic.js
@@ -23,7 +23,6 @@ export class XdvCarouselBasic extends XdvStringToKebabCase(XdvFetchGetDataMixin(
   } 
   
    firstUpdated () {
-    this.apiUrl = this.dataset.apiUrl
     (async() => {
       await this.getData()
       
